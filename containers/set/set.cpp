b9f135f9efbcc1ba661ee41362a5bcfb06644ccf
Nikolai Baschinski <nikolai.baschinski@web.de>
2025-11-09 10:29:57 +0100

    update set and unordered_set

--- containers/set/set.cpp
+++ containers/set/set.cpp
@@ -2,7 +2,9 @@
 #include <set>
 
 // std::set is an associative container that contains a sorted set of unique objects of type Key.
-// Sorting is done using a key comparison function
+// Sorting is done using the key comparison function Compare.
+// Search, removal, and insertion operations have logarithmic complexity.
+// Sets are usually implemented as Red–black trees.
 
 using namespace std;
 
@@ -12,10 +14,17 @@ int main()
 
     for (int n : numbers)
         std::cout << n << " ";
-
     cout << endl;
+
     cout << numbers.contains(5) << endl;
     cout << numbers.contains(9) << endl;
 
-    numbers.find(78) != numbers.end() ? cout << "found" : cout << "not found";
+    // search with logarithmic complexity
+    numbers.find(78) != numbers.end() ? cout << "found \n" : cout << "not found \n";
+
+    // descending traversing
+    for (auto it = numbers.rbegin(); it != numbers.rend(); ++it)
+        std::cout << *it << " ";
+
+
 }
--- containers/unordered_set/unordered_set.cpp
+++ containers/unordered_set/unordered_set.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <unordered_set>
 
+// std::unordered_set is an associative container that contains a set of unique
+// objects of type Key.Search, insertion, and removal have average constant - time complexity.
+
+// Internally, the elements are not sorted in any particular order, but organized into buckets.
+// Which bucket an element is placed into depends entirely on the hash of its value.
+// This allows fast access to individual elements, since once a hash is computed,
+// it refers to the exact bucket the element is placed into.
+
 using namespace std;
 
 int main()
@@ -9,8 +17,9 @@ int main()
 
     for (auto e : s)
         cout << e << " ";
+    cout << endl;
 
-    cout << s.bucket_count() << endl;
-    cout << s.load_factor() << endl;
+    cout << "bucket_count: " << s.bucket_count() << endl;
+    cout << "load_factor: " << s.load_factor() << endl;
     std::cout << "Hello World!\n";
 }
