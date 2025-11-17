0496bca417be7392ad3ad8b8e5d21b81d1862390
Nikolai Baschinski <nikolai.baschinski@web.de>
2025-11-17 16:28:35 +0100

    added numeric midpoint

---
+++ numeric/midpoint/midpoint.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <numeric>
+
+using namespace std;
+
+int main()
+{
+    cout << midpoint(10, 100) << endl;
+    cout << midpoint(-7.0, -8.0) << endl;
+
+    std::cout << "Hello World!\n";
+}
